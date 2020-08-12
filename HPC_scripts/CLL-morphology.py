16de146121ced5af01653ba13e392281be5372ae
vladchimescu <vladhkim@gmail.com>
2020-08-12 19:33:38 +0200

    added an HPC script for CLL morphologica profiling

---
+++ HPC_scripts/CLL-morphology.py
@@ -0,0 +1,94 @@
+#!/usr//bin/env python3
+'''
+Script for single-cell morphology feature
+extraction from segmented CLL nuclei
+'''
+import javabridge
+import bioformats as bf
+import numpy as np
+import pandas as pd
+import os
+import sys
+import re
+import h5py
+from ast import literal_eval
+from bioimg import ImgX, load_image_series, read_bbox
+
+if __name__ == '__main__':
+    javabridge.start_vm(class_path=bf.JARS)
+
+    # path to CLL data
+    # path = '/Volumes/gitlab/microscopy/data/Sophie/'
+    path = sys.argv[1]
+    # plate identifier
+    # plate = '180306_Plate1'
+    plate = sys.argv[2]
+    wellnum = int(sys.argv[3]) - 1
+    print("Processing plate: " + str(plate))
+
+    imgdir = os.path.join(path, 'CLL')
+    evdir = os.path.join(path, 'Evaluations')
+    
+    for f in os.listdir(imgdir):
+        if re.search(plate, f):
+            screen_id = f
+
+    imgdir = os.path.join(imgdir, screen_id, 'Images')
+    evdir = os.path.join(evdir, screen_id)
+
+    evname = [s for s in os.listdir(evdir)
+              if re.match('Evaluation', s) is not None]
+    evdir = os.path.join(evdir, evname[0])
+
+    # load the bounding box file for the whole plate
+    eval_df = pd.read_table(os.path.join(evdir,
+                                         'Objects_Population - CLL cell nuclei.txt'), skiprows=9)
+    eval_df['Row'] = eval_df['Row'].astype(str)
+    eval_df['Column'] = eval_df['Column'].astype(str)
+    eval_df['Field'] = eval_df['Field'].astype(str)
+
+    eval_df = eval_df.assign(well=lambda x: 'r' + x['Row'].str.zfill(2) + 'c' + x['Column'].str.zfill(2))
+    eval_df = eval_df.assign(wellpos=lambda x: x['well'] + 'f' + x['Field'].str.zfill(2))
+    eval_df = eval_df[['well','wellpos', 'Bounding Box', 'X', 'Y']]
+    eval_df[['xmin','ymin','xmax','ymax']] = pd.DataFrame(eval_df['Bounding Box'].apply(lambda x: list(literal_eval(x))).tolist())
+
+    all_wells = eval_df['well'].unique()
+    well = all_wells[wellnum]
+    fnames = [f for f in os.listdir(imgdir) if '.tiff' in f]
+   
+    imgdata = []
+    for fview in range(1,4):
+       wellpos =  well + 'f' + str(fview).zfill(2)
+       wfiles = [f for f in fnames if wellpos in f and '(2)' not in f]
+       imgstack = load_image_series(path=imgdir,
+                                    imgfiles=[w for w in wfiles if 'ch1' in w])
+       hoechst = np.max(imgstack, axis=0)
+       imgstack = load_image_series(path=imgdir,
+                                    imgfiles=[w for w in wfiles if 'ch2' in w])
+       ly = np.max(imgstack, axis=0)
+
+       well_df = eval_df[eval_df['wellpos']==wellpos]
+       if well_df.shape[0]:
+           rmax, cmax = hoechst.shape
+           bbox = read_bbox(df=well_df, rmax=rmax,
+                            cmax=cmax,
+                            columns=['ymin','xmin','ymax','xmax'],
+                            pad=5)
+
+           imgx = ImgX(img=np.stack([hoechst, ly], axis=-1), 
+                bbox=bbox,
+                n_chan=['Hoechst', 'Lysosomal'])
+           imgx.compute_props()
+           img_df = imgx.get_df().copy()
+           imgdata.append(img_df)
+
+
+    X_df = pd.concat(imgdata).reset_index(drop=True)
+    
+    outdir = os.path.join('CLLdata', plate)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    X_out.to_csv(os.path.join(outdir, well + '.csv'),
+                 index=False)
+    javabridge.kill_vm()
